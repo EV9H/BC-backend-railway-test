a468a7ddc39533a452ad0325d920b6b43a98ac17
ev9h <hh2266@nyu.edu>
2023-01-10 02:09:05 -0500

    udp

--- backend/settings.py
+++ backend/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from datetime import timedelta
 
 
@@ -165,13 +165,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "backend/static") 
-STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
